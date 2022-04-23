2c5a5770a1773687a15224cbd7cbec6ca512846e
kerogik <igoreklesch@gmail.com>
2022-04-23 17:19:56 +0300

    hz

--- dixotomia.cpp
+++ dixotomia.cpp
@@ -27,16 +27,16 @@ double dixot(double a,double b,double eps,int l) {
     }
     return c;
 }
-int main() {
-    double a,b,eps,l;
-    std::cout << "Введите значение левой границы" << std::endl;
-    std::cin >> a;
-    std::cout << "Введите значение правой границы" << std::endl;
-    std::cin >> b;
-    std::cout << "Введите значение погрешности" << std::endl;
-    std::cin >>eps;
-    std::cout << "Что нужно найти? \n 1-максимум \n 2-минимум" << std::endl;
-    std::cin >>l;
-    std::cout << dixot(a,b,eps,l) << std::endl;
-}
+// int main() {
+//     double a,b,eps,l;
+//     std::cout << "Введите значение левой границы" << std::endl;
+//     std::cin >> a;
+//     std::cout << "Введите значение правой границы" << std::endl;
+//     std::cin >> b;
+//     std::cout << "Введите значение погрешности" << std::endl;
+//     std::cin >>eps;
+//     std::cout << "Что нужно найти? \n 1-максимум \n 2-минимум" << std::endl;
+//     std::cin >>l;
+//     std::cout << dixot(a,b,eps,l) << std::endl;
+// }
 
---
+++ graphics.cpp
@@ -0,0 +1,30 @@
+#include <sciplot/sciplot.hpp>
+using namespace sciplot;
+
+int main(int argc, char** argv) {
+    //creating a vector with values from 0 to pi with precision of pi/200 (?)
+    Vec x = linspace(0.0, PI, 200);
+    
+    //plot obj
+    Plot plot;
+
+    //set x and y labels
+    plot.xlabel("x");
+    plot.ylabel("y");
+
+    //set ranges
+    plot.xrange(0.0, PI);
+    plot.yrange(0.0, 1);
+    
+    //legend
+    plot.legend()
+        .atOutsideBottom()
+        .displayHorizontal()
+        .displayExpandWidthBy(2);
+
+    //plot sinx
+    plot.drawCurve(x, std::sin(x)).label("sinx");
+    
+    //show
+    plot.show();
+}
