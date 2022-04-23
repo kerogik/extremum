fe62df65b6a4a8ae16fafcfd86f0212dcbc4cd7e
kerogik <igoreklesch@gmail.com>
2022-04-23 20:37:46 +0300

    aaaaa

--- graphics.cpp
+++ graphics.cpp
@@ -1,10 +1,16 @@
 #include <sciplot/sciplot.hpp>
 using namespace sciplot;
 
-int graphics(double left_border = 0.0, double right_border = 5.0) {
+double f(double x) {
+    return sin(x);
+}
+
+int graphics(double left_border = 0.0, double right_border = 5.0, double result_dichotomy = 0.0) {
     //creating a vector with values from 0 to pi with precision of pi/200 (?)
     Vec x = linspace(left_border, right_border, 200);
-    
+    Vec vec_for_dot = linspace(result_dichotomy, result_dichotomy, 1);
+    Vec xBox = linspace(result_dichotomy-0.01, result_dichotomy+0.01, 200);
+    Vec yBox = linspace(std::sin(result_dichotomy)-0.01, std::sin(result_dichotomy)+0.01, 200);
     //plot obj
     Plot plot;
 
@@ -14,7 +20,7 @@ int graphics(double left_border = 0.0, double right_border = 5.0) {
 
     //set ranges
     plot.xrange(left_border, right_border);
-    plot.yrange(-10, 10);
+    plot.yrange(-1.5, 1.5);
     
     //legend
     plot.legend()
@@ -24,7 +30,13 @@ int graphics(double left_border = 0.0, double right_border = 5.0) {
 
     //plot sinx
     plot.drawCurve(x, std::sin(x)).label("sin(x)");
-    
+    plot.drawDots(vec_for_dot, std::sin(vec_for_dot)).label("extremum : "+ std::to_string(result_dichotomy)); 
+    plot.drawBoxes(xBox, yBox)
+        .fillSolid()
+        .fillColor("red")
+        .fillIntensity(0.5)
+        .label("pointer to  the dichotomy extremum");
+    plot.boxWidthRelative(0.3);
     //show
     plot.show();
     return 0;
--- main.cpp
+++ main.cpp
@@ -2,15 +2,16 @@
 #include "dixotomia.cpp"
 
 int main(int argc, char **argv) {
-    double a,b,eps,l;
+    double left_border,right_border,eps,choice;
     std::cout << "Введите значение левой границы" << std::endl;
-    std::cin >> a;
+    std::cin >> left_border;
     std::cout << "Введите значение правой границы" << std::endl;
-    std::cin >> b;
+    std::cin >> right_border;
     std::cout << "Введите значение погрешности" << std::endl;
     std::cin >>eps;
     std::cout << "Что нужно найти? \n 1-максимум \n 2-минимум" << std::endl;
-    std::cin >>l;
-    std::cout << dixot(a,b,eps,l) << std::endl;
-    graphics();
+    std::cin >>choice;
+    double result_dichotomy = dixot(left_border,right_border,eps,choice);
+    std::cout << result_dichotomy << std::endl;
+    graphics(left_border, right_border, result_dichotomy);
 }
