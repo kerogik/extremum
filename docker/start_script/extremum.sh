90f45a74646672fc5d800464c6c503dc3a23ab71
kerogik <igoreklesch@gmail.com>
2022-05-26 13:10:40 +0300

    upd docker

--- docker/start_script/extremum.sh
+++ docker/start_script/extremum.sh
@@ -7,3 +7,6 @@ mv /plot_fib.pdf /usr/share/nginx/html/plot_fib.pdf
 mv /precision_dichotomy.pdf /usr/share/nginx/html/precision_dichotomy.pdf
 mv /precision_section.pdf /usr/share/nginx/html/precision_section.pdf
 mv /precision_fib.pdf /usr/share/nginx/html/precision_fib.pdf
+mv /iteration-precision-dichotomy.pdf /usr/share/nginx/html/iteration-precision-dichotomy.pdf
+mv /iteration-precision-section.pdf /usr/share/nginx/html/iteration-precision-section.pdf
+mv /iteration-precision-fib.pdf /usr/share/nginx/html/iteration-precision-fib.pdf
